4dc7fa9e50a1ba3159291510f7538426b34fdce3
O.D.E.I.T <62390626+I-Sta@users.noreply.github.com>
2020-04-04 12:39:26 -0400

    updated-nav-bar
    -edit and + button removed

--- simpsonJSON/simpsonJSON/MasterViewController.swift
+++ simpsonJSON/simpsonJSON/MasterViewController.swift
@@ -20,10 +20,10 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        navigationItem.leftBarButtonItem = editButtonItem
+//        navigationItem.leftBarButtonItem = editButtonItem
        
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
+//        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+//        navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -34,10 +34,10 @@ class MasterViewController: UITableViewController {
                
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
-        super.viewWillAppear(animated)
-    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+//        super.viewWillAppear(animated)
+//    }
     func simpsonJsonParse() {
         if let url = URL(string: "http://api.duckduckgo.com/?q=simpsons+characters&format=json") {
             URLSession.shared.dataTask(with: url) { data, response, error in
@@ -94,7 +94,7 @@ class MasterViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = simsonObjects[indexPath.row] as! SimpsonCharact
+                let object = simsonObjects[indexPath.row] as SimpsonCharact
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
